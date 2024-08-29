33882f47f5bb510156647f40e90d1a9f9bcec75a
JsuisSayker <killian.trouve@epitech.eu>
2024-08-29 13:00:38 +0200

    [ADD] loginStep
    add the first step of the login process, by adding the start of the route and the views

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use App\Http\Controllers\GraphicController;
 use App\Http\Controllers\TeamsController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminSessionController;
 
 Route::view('/', 'home');
 Route::resource('teams', TeamsController::class);
 Route::get('/graphic', [GraphicController::class, 'index'])->name('graphic.index');
 Route::view('/rules', 'rules');
 Route::view('/collection', 'collection');
+
+Route::get("/login", [AdminSessionController::class, 'create']);
